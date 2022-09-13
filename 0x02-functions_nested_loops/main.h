2ed0748ff81c3bfac99e785363c2c1748ab95a6d
thezillionairedev <thezillionairedev@gmail.com>
2022-09-13 07:59:20 -0700

    Updated main.h files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+/**
+ * File: main.h
+ *
+ * Author: Zillionaire
+ *
+ * Desc: Header files containig function declarations
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
