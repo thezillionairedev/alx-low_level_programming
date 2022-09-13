0fce02eae524e3c3dd4dfb14db2b045ba3499fd1
thezillionairedev <thezillionairedev@gmail.com>
2022-09-13 04:23:10 -0700

    Prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[20] = "_putchar";
+
+	_putchar(a);
+
+	_putchar('\n');
+	return (0);
+}
