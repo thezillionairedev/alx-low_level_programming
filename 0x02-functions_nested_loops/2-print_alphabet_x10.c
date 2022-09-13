bc417a4b80c395955948404bc577106bda2986df
thezillionairedev <thezillionairedev@gmail.com>
2022-09-13 07:22:48 -0700

    Prints alphabet 10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  *
  * Description - Prints 10 times the alphabet
  *
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 'a'; j = 'z'; j++)
+		for (j = 97; j = 122; j++)
 		{
 			_putchar(j);
 		}
