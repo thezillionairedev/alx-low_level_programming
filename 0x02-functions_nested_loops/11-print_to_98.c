b415f5488638eab22b4da4cce23b1cf43300efdb
thezillionairedev <thezillionairedev@gmail.com>
2022-09-13 13:31:18 -0700

    Prints natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,6 +15,7 @@ void print_to_98(int n)
 		{
 			printf("%d ", n);
 		}
+	}
 	else
 	{
 		for (n = n; n > 98; n--)
@@ -22,6 +23,5 @@ void print_to_98(int n)
 			printf("%d ", n);
 		}
 	}
-	printf("%d\n");
-	}
+	printf("%d\n", n);
 }
