0130d2ba19e017e0b8b60d7cbd8a2166422c1cc2
thezillionairedev <thezillionairedev@gmail.com>
2022-09-15 13:30:44 -0700

    Program to print fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number
+ *
+ * Return: Buzz if number is 3 and Fizz if 5
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
