dab5c73e128cd27f469f309c73ee29be51668add
thezillionairedev <thezillionairedev@gmail.com>
2022-09-15 14:05:43 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz\n");
+			printf("FizzBuzz\n ");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz\n");
+			printf("Fizz\n ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz\n");
+			printf("Buzz\n ");
 		}
 		else
 		{
-			printf("%d\n", n);
+			printf("%d\n ", n);
 		}
 	}
 	return (0);
