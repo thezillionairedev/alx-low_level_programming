66ee102fbf2bcde9decebdffdb8e5218cee12153
thezillionairedev <thezillionairedev@gmail.com>
2022-09-15 14:21:20 -0700

    Prints a traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints traingle
+ * @size: size odf traingle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
+		for (j--; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i < (size - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
+
