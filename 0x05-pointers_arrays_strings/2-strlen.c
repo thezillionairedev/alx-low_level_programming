8af7c17964c2a58db967427e08effb1d9ddf1fa4
thezillionairedev <thezillionairedev@gmail.com>
2022-09-19 02:58:12 -0700

    Function the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - returns length of a string
  * @s: sting to be evaluated
- * Return: 0
+ * Return: length
  */
 
 int _strlen(char *s)
@@ -11,5 +11,6 @@ int _strlen(char *s)
 	int len = 0;
 
 	while (*(s + len) != 0)
-		len++
+		len++;
+	return (len);
 }
