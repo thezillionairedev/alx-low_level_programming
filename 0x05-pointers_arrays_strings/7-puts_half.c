dea3e1ab8edfb3f9f83b441a38376238e1153f4a
thezillionairedev <thezillionairedev@gmail.com>
2022-09-19 04:24:34 -0700

    Prints half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,21 @@
 
 void puts_half(char *str)
 {
-	int len, n, i;
+	int count = 0, i;
 
-	for (len = 0; str[len] != '\0'; len++)
+	while (count >= 0)
 	{
-		if (len % 2 == 0)
-		{
-			for (i = len / 2; str[i] != '\0'; i++)
-				_putchar(str[i]);
-		}
-		else if (len % 2)
-		{	
-			for (n = (len - 1) / 2; n < len - 1; n++)
-					_putchar(str[n + 1]);
-		}
+		if (str[count] == '\0')
+			break;
+		count++;
 	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+	
+	for (i++; i < count; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
