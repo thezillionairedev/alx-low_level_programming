6235bb3db7ce9d64b581402850fa46a554b0f638
thezillionairedev <thezillionairedev@gmail.com>
2022-09-28 03:57:30 -0700

    returns 1 if the input integer is a prime number, otherwise
    return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,19 +3,18 @@
 /**
  * is_prime - detects if an input number is a prime number
  * @n: input number
- * @x: iterator
- * Return: 1 if n is a prime number. 0 if n is not a prime number
+ * @i: iterator
+ * Return: 1 or 0
  */
-int is_prime(unsigned int n, unsigned int x)
+int is_prime(int n, int i)
 {
-	if (n % x == 0)
-	{
-		if (n == x)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + is_prime(n, c + 1));
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (is_prime(n, i + 1));
 }
 /**
  * is_prime_number - detects if an input number is a prime number
@@ -24,11 +23,5 @@ int is_prime(unsigned int n, unsigned int x)
  */
 int is_prime_number(int n)
 {
-	if (n == 0)
-		return (0);
-	if (n < 0)
-		return (0);
-	if (n == 1)
-		return (0);
-	return (is_prime(n, 2));
+	return (is_prime(n, 1));
 }
