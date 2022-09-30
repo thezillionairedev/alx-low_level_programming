13f26ff871d5b704db0c16194b6181c69a17ee64
thezillionairedev <thezillionairedev@gmail.com>
2022-09-30 12:12:56 -0700

    Initial comit

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - unction that computes the absolute value of an integer
+ * @n: Integer
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
