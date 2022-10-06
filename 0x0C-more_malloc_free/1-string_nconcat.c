42203947816e9e02a8ab5ad35b8e2053772f3906
thezillionairedev <thezillionairedev@gmail.com>
2022-10-06 10:40:53 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; i < concat; i++)
+	for (i = 0; i < lensconcat; i++)
 	{
 		if (i < lens1)
 			concat[i] = s1[i];
