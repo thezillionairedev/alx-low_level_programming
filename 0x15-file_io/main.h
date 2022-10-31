663fd65afcd81feeb5658c5a78747cfd85169db6
thezillionairedev <thezillionairedev@gmail.com>
2022-10-31 08:11:55 -0700

    Update header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
